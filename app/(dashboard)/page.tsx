50f011a38f5f6f3cb568fdb5f9d3664d1dc68557
git112 <23it021@charusat.edu.in>
2025-01-16 21:32:39 +0530

    Initial commit

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,23 @@
+// import { Button } from "@/components/ui/button";
+// import Image from "next/image";
+// import { UserButton } from "@clerk/nextjs";
+
+// export default function Home() {
+//   return (
+//     <UserButton afterSignOutUrl="/"/>
+//   );
+// }
+
+
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div>
+      Dashboard Page
+      
+    </div>
+  );
+}
